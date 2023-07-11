d2955aa952fc81420ed6e922cd35ba63da28e9f8
jklenzing <jklenzing@users.noreply.github.com>
2023-07-11 16:36:08 -0400

    add plots

--- tids.py
+++ tids.py
@@ -1,19 +1,61 @@
 # -*- coding: utf-8 -*-
 """Quick and simple view of TIDS in DE2 data."""
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 import pysat
 
 from funcs import calculate_delta
 
-lang = pysat.Instrument('de2', 'lang', use_header=True, strict_time_flag=False)
+orbit_info = {'index': 'OrbitNumber', 'kind': 'orbit'}
+
+lang = pysat.Instrument('de2', 'lang', use_header=True, strict_time_flag=False,
+                        orbit_info=orbit_info)
 lang.custom_attach(calculate_delta,
                    kwargs={'labels': ['plasmaDensity'],
                            'normalize': True})
 
-nacs = pysat.Instrument('de2', 'nacs', use_header=True)
+nacs = pysat.Instrument('de2', 'nacs', use_header=True, strict_time_flag=False,
+                        orbit_info=orbit_info)
 nacs.custom_attach(calculate_delta,
                    kwargs={'labels': ['O_density', 'N2_density'],
                            'normalize': True})
 
-lang.load(1983, 22)
-nacs.load(1983, 22)
+rpa = pysat.Instrument('de2', 'rpa', use_header=True, strict_time_flag=False)
+rpa.custom_attach(calculate_delta,
+                  kwargs={'labels': ['ionDensity'],
+                          'normalize': True})
+rpa.custom_attach(calculate_delta,
+                  kwargs={'labels': ['x', 'y', 'z'],
+                          'normalize': False})
+doy = 25
+lang.load(1983, doy)
+nacs.load(1983, doy)
+rpa.load(1983, doy)
+
+orbits = np.unique(lang['OrbitNumber'])
+
+plt.close('all')
+for orbit in orbits:
+    # Choose orbit number and low altitudes
+    ind = (nacs['OrbitNumber'] == orbit) * (nacs['alt'] < 400)
+    if sum(ind)  > 100:
+        t0 = nacs.index[ind][0]
+        t1 = nacs.index[ind][-1]
+
+        fig = plt.figure(figsize=[14, 6])
+        plt.plot(lang['delta_plasmaDensity_norm'][t0:t1],
+                 label='LANG: delta Ne')
+        plt.plot(rpa['delta_ionDensity_norm'][t0:t1], label='RPA: delta Ni')
+        plt.plot(nacs['delta_O_density_norm'][t0:t1], label='NACS: delta O')
+        plt.plot(nacs['delta_N2_density_norm'][t0:t1], label='NACS: delta N2')
+        plt.legend()
+        plt.grid()
+        plt.ylim([-0.5, 0.5])
+
+        plt.ylabel('Relative change (normalized)')
+        plt.title('DE2 -- {:}/{:}/{:} -- Orbit {:}'.format(t0.date().year,
+                                                           t0.date().month,
+                                                           t0.date().day,
+                                                           orbit))
